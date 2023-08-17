5506e37fa5e158820004f6dcd1d9f4b4853d7933
Kaj <tassi100@mail.chapman.edu>
2023-08-16 18:12:53 -0700

    afterwork tweaks

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -2,23 +2,28 @@ import "./Styling.css";
 
 function Scores({ scores }) {
   return (
-    <section className="scores">
-      <div>
-        <span>Player Wins</span>
-        <br />
-        <span>{scores.x}</span>
-      </div>
-      <div>
-        <span>Draws</span>
-        <br />
-        <span>{scores.tie}</span>
-      </div>
-      <div>
-        <span>AI Wins</span>
-        <br />
-        <span>{scores.o}</span>
-      </div>
-    </section>
+    <body>
+      <title className="scores">
+        <h1>Scoreboard</h1>
+      </title>
+      <section className="scores">
+        <div>
+          <span>Player Wins</span>
+          <br />
+          <span>{scores.x}</span>
+        </div>
+        <div>
+          <span>Draws</span>
+          <br />
+          <span>{scores.tie}</span>
+        </div>
+        <div>
+          <span>AI Wins</span>
+          <br />
+          <span>{scores.o}</span>
+        </div>
+      </section>
+    </body>
   );
 }
 
